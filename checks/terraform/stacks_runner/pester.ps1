883ca6dc0a9bfa00e85aaeeb79df48f45c23b246
Szymon Marciniak <szymon.marciniak@ensono.com>
2026-01-07 16:33:02 +0100

    Init first check version

---
+++ checks/terraform/stacks_runner/pester.ps1
@@ -0,0 +1,58 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [hashtable] $parentConfiguration
+)
+
+BeforeDiscovery {
+    # installing dependencies
+    # to avoid a potential clash with the YamlDotNet libary always load the module 'powershell-yaml' last
+    Install-PowerShellModules -moduleNames ("powershell-yaml")
+
+    $configurationFile = $parentConfiguration.configurationFile
+
+    # configuration
+    $configurationFile = $parentConfiguration.configurationFile
+    $checkConfiguration = (Get-Content -Path $configurationFile | ConvertFrom-Yaml).($parentConfiguration.checkName)
+
+    # building the discovery objects
+    $discovery = $checkConfiguration
+    $repositories = $discovery.repositories
+    Write-Host ($discovery | Out-String)
+    Write-Host ($repositories | Out-String)
+} 
+
+Describe $parentConfiguration.checkDisplayName -ForEach $discovery {
+
+    Context "Repo: <_.repo_url>" -ForEach $repositories {
+        BeforeAll {
+            git clone $_.repo_url
+        }
+#
+#        BeforeEach {
+#            @"
+#            terraform {
+#                # https://developer.hashicorp.com/terraform/language/expressions/version-constraints
+#                required_version = "$_"
+#            }
+#"@ | Set-Content -Path $testFilePath -Force
+#        }
+#
+#        It "'terraform init' should return an Exit Code of 0" {
+#            terraform init
+#            $LASTEXITCODE | Should -Be 0
+#        }
+#
+#        AfterEach {
+#            Remove-Item -Path $testFilePath -Force
+#        }
+#
+#        AfterAll {
+#            Clear-Variable -Name testFilePath
+#        }
+#    }
+
+#    AfterAll {
+#        Write-Information -MessageData ("`nInstalled Terraform version: {0}" -f $(terraform --version))
+#        Write-Information -MessageData ("`nRunbook: {0}`n" -f $_.runbook)
+    }
+}

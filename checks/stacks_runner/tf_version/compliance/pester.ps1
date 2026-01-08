c2c61804d50424c6d979e7e7b09729e03f43cb02
Szymon Marciniak <szymon.marciniak@ensono.com>
2026-01-08 14:48:18 +0100

    Move Stacks runner tf version check to other dir

--- checks/stacks_runner/tf_version/compliance/pester.ps1
+++ checks/stacks_runner/tf_version/compliance/pester.ps1
@@ -17,7 +17,16 @@ BeforeDiscovery {
     # building the discovery objects
     $discovery = $checkConfiguration
     $repositories = $discovery.repositories
+    $env_access_token=$env:ADO_ACCESS_TOKEN
+
+    $accessToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($env:ADO_ACCESS_TOKEN)"))
+#    Write-Host "Access token after parsing:"
+#    Write-Host ($accessToken | Out-String)
+
+    Write-Host "Discovery:"
     Write-Host ($discovery | Out-String)
+
+    Write-Host "Repositories:"
     Write-Host ($repositories | Out-String)
 } 
 
@@ -25,7 +34,16 @@ Describe $parentConfiguration.checkDisplayName -ForEach $discovery {
 
     Context "Repo: <_.repo_url>" -ForEach $repositories {
         BeforeAll {
-            git clone $_.repo_url
+#            $header="Authorization: Bearer $accessToken"
+#            git -c http.extraheader=$header clone $_.repo_url
+#             git clone https://$($env:ADO_ACCESS_TOKEN)@${($_.repo_url -replace "^https://","")}
+            git clone "https://$($env:ADO_ACCESS_TOKEN)@dev.azure.com/PayUK/Pay.UK%20API%20Platform/_git/payuk-iac"
+            git clone "https://$($env:ADO_ACCESS_TOKEN)@dev.azure.com/PayUK/Pay.UK%20API%20Platform/_git/sre-cdm-checks"
+        }
+
+        It "Cloned repository '<_.repo_url>' should exist" {
+            $repoName = ($_.repo_url).Split("/")[-1] -replace ".git$",""
+            Test-Path -Path "./$repoName" | Should -Be $true
         }
 #
 #        BeforeEach {
